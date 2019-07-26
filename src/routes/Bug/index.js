e7793c722e1e64bb41c5f9deb1a650e24d75e018
Anna Mistykova <a.mistyukova@adriver.ru>
2019-07-26 15:49:17 +0300

    table template

--- src/routes/Bug/index.js
+++ src/routes/Bug/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 
 class Bug extends Component {
   render () {
     return (
       <Container>
         <h2>Ошибка</h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan='2'>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
     )
   }
